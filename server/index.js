e3e8154ff40169f16d0b3fc89086fcf40cea0420
Serghey Soloviov <thatmean@gmail.com>
2018-08-20 21:58:33 +0300

    balance + balanceTimeseries

--- server/index.js
+++ server/index.js
@@ -2,31 +2,58 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient
-const mongoConf = require('./_utils/_private.json').mongo
+const pr = require('../private/keys.json').keys
+const mongoConf = require('../private/mongo.json').mongo
+const ccxt = require ('ccxt')
 const url = 'mongodb://'+mongoConf.username+':'+mongoConf.password+'@'+mongoConf.host+':'+mongoConf.port+'/'+mongoConf.db+'?authSource=admin'
 app.use(bodyParser.json())
+const localMongoUrl = "mongodb://192.168.99.100:32768/client"
+const cors = require('cors')
+app.use(cors())
+
+
+
 let db
+let localMongo
+
+///////////////
+//globals start
+///////////////
+global.COINMARKETCAP
+global.BALANCE
+
+///////////////
+//globals end
+///////////////
+
+var updateBalance = require('./core_components/updateBalance')
+var updateCoinmarketcap = require('./core_components/updateCoinmarketcap')
 
 const main = async () => {
-  try {
- 	db = await MongoClient.connect(url)
-    
-    // balance (pie chart)
-    app.get('/balance', function (req, res) {
-		res.json('Хав аюе ду')
-    })
-    // balance_history (linechart)
+	try {
+		db = await MongoClient.connect(url)
+		localMongo = await MongoClient.connect(localMongoUrl)
 
-    // my_traders_short
-    // my_traders_all
+		try { updateBalance(localMongo, pr, 10000) } catch(err) { console.log(err) }
+		try { updateCoinmarketcap(db) } catch(err) { console.log(err) }
 
-    // open_orders ()
 
-    // TRADE API
-    // place
-    // cancel
-    // replace
-  } catch (err) { }
+		// balance (pie chart)
+		app.get('/balance', function (req, res) {
+			res.json(global.BALANCE)
+		})
+		// balance_history (linechart)
+
+		// my_traders_short
+		// my_traders_all
+
+		// open_orders ()
+
+		// TRADE API
+		// place
+		// cancel
+		// replace
+	} catch (err) { }
 }
 main()
 
