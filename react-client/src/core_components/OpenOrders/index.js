f6deff765531c6a9dd71ee168ae6c7a3a1d57e8d
Serghey Soloviov <thatmean@gmail.com>
2018-09-05 19:22:23 +0300

    prichesony

--- react-client/src/core_components/OpenOrders/index.js
+++ react-client/src/core_components/OpenOrders/index.js
@@ -6,56 +6,64 @@ import { inject, observer } from 'mobx-react'
 @observer
 class OpenOrders extends React.Component {
   render() {
+    var colWidth = {
+      width: 220
+    }
     const {OpenOrdersStore} = this.props
     return (
       <div>
-        <h5>orders</h5>
-
-        <table>
-          <thead>
-            <tr>
-              <th>orderInfo</th>
-              <th>priceInfo</th>
-              <th>volumeInfo</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            _.map(OpenOrdersStore.openOrders, (item, i) => {
-              return (
+        <div className="el-table el-table--fit el-table--enable-row-hover myTrades-table">
+          <div className="el-table__header-wrapper">
+            <table classMane="el-table__header">
+              <thead>
                 <tr>
-                  <td>
-                    id: {item['id']}<br/>
-                    status: {item['status']}<br/>
-                    type: {item['type']}<br/>
-                    side: {item['side']}<br/>
-                    symbol:{item['symbol']}<br/>
-                    date: {item['datetime']}<br/>
-                  </td>
-                  <td>
-                    price: {item['price']}<br/>
-                  </td>
-                  <td>
-                    amount: {item['amount']}<br/>
-                    filled: {item['filled']}<br/>
-                    remaining: {item['remaining']}<br/>
-                    lastTrade: {item['lastTradeTimestamp']}
-                  </td>
-                  <td>
-                    close<br/>
-                    change
-                  </td>
-
+                  <th className="is-leaf" style={colWidth}><div className="cell">orderInfo</div></th>
+                  <th className="is-leaf" style={colWidth}><div className="cell">priceInfo</div></th>
+                  <th className="is-leaf" style={colWidth}><div className="cell">volumeInfo</div></th>
+                  <th className="is-leaf" style={colWidth}><div className="cell"></div></th>
                 </tr>
-              )
-            })
+              </thead>
+            </table>
+          </div>
+          <div className="el-table__body-wrapper">
+            <table className="el-table__body">
+              <tbody>
+              {
+                _.map(OpenOrdersStore.openOrders, (item, i) => {
+                  return (
+                    <tr>
+                      <td>
+                        id: {item['id']}<br/>
+                        status: {item['status']}<br/>
+                        type: {item['type']}<br/>
+                        side: {item['side']}<br/>
+                        symbol:{item['symbol']}<br/>
+                        date: {item['datetime']}<br/>
+                      </td>
+                      <td>
+                        price: {item['price']}<br/>
+                      </td>
+                      <td>
+                        amount: {item['amount']}<br/>
+                        filled: {item['filled']}<br/>
+                        remaining: {item['remaining']}<br/>
+                        lastTrade: {item['lastTradeTimestamp']}
+                      </td>
+                      <td>
+                        close<br/>
+                        change
+                      </td>
+
+                    </tr>
+                  )
+                })
 
 
-          }
-          </tbody>
-        </table>
-
+              }
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
--- react-client/src/core_components/Stocks.js
+++ react-client/src/core_components/Stocks.js
@@ -6,20 +6,28 @@ import { inject, observer } from 'mobx-react'
 @observer
 class Stocks extends React.Component {
   render() {
+    var colWidth = {
+      width: 180
+    }
     const {GlobalStore, type} = this.props
     return (
       <div>
-        <table>
-          <tbody>
-            {
-              _.map(GlobalStore.stocks, (stock) => {
-                return <tr key={stock}>
-                  <td><a href="#" onClick={this.handleClick.bind(this, stock.name)}>{stock.name}</a></td>
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
+        <div className="el-table el-table--fit noHeader-table">
+          <div className="el-table__body-wrapper">
+            <table className="el-table__body">
+
+              <tbody>
+                {
+                  _.map(GlobalStore.stocks, (stock) => {
+                    return <tr key={stock} lassName="el-table__row">
+                      <td><div className="cell"><a href="#" onClick={this.handleClick.bind(this, stock.name)}>{stock.name}</a></div></td>
+                    </tr>
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
