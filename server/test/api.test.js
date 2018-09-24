e83bf3b7e2455587c39e58fd50509677d16a60a2
Serghey Soloviov <thatmean@gmail.com>
2018-09-24 16:40:46 +0300

    test +, orderbook api fix

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -1,22 +1,29 @@
 var getMyTrades = require('../core_components/getMyTrades')
 var {getPairs} = require('../core_components/getPairs')
 var {getStocks} = require('../core_components/getStocks')
+var {getOrderBook} = require('../core_components/getOrderBook')
 
 global.PAIRS = {}
+global.ORDERBOOK = {}
 
-
-describe('getPairs test find ETH/BTC', () => {
-  it('matches even if received contains additional elements', async () => {
-    
+describe('API tests', () => {
+  it('getPairs(ETH/BTC)', async () => {
     var result = await getPairs('binance')
     expect(result).toEqual(expect.arrayContaining(['ETH/BTC']))
   })
-})
-
-describe('getStocks test find binance', () => {
-  it('matches even if received contains additional elements', async () => {
+  it('getStocks(binance)', async () => {
     var result = await getStocks()
     expect(result['BINANCE']['name']).toEqual('BINANCE')
   })
-})  
+  it('getOrderBook(binance, ETH_BTC) is object', async () => {
+    var result = await getOrderBook('binance', 'ETH_BTC')
+    expect(result).toBeInstanceOf(Object)
+  })
+  it('getOrderBook(binance, ETH_BTC) have asks & bids', async () => {
+    var result = await getOrderBook('binance', 'ETH_BTC')
+    expect(Object.keys(result)).toEqual(
+      expect.arrayContaining([ 'bids', 'asks' ])
+    )
+  })
+})
 
