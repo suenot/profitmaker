3a01ea524cc018f690798ce66c5d7c128fe51108
Serghey Soloviov <thatmean@gmail.com>
2018-12-17 12:35:37 +0200

    vue

--- vue-client/src/App/App.js
+++ vue-client/src/App/App.js
@@ -9,7 +9,7 @@
 // @Component
 // export default class App extends Vue {
 //     state = new ViewModel()
-//     mounted() { 
+//     mounted() {
 //         console.log('work')
 //     }
 // }
@@ -25,36 +25,53 @@
 // })
 
 
+// export default {
+//   // fromMobx: {
+//   //   testModel
+//   // },
+//   data: () => ({
+//     dialog: false,
+//     drawer: null,
+//     drawerRight: null,
+//     newId: '3',
+//     dashboards: {
+//       '1': { id: '1', icon: 'view_quilt', text: 'Intuition' },
+//       '2': { id: '2', icon: 'view_list', text: 'Refactuiton' }
+//     }
+//   }),
+//   props: {
+//     source: String
+//   },
+//   methods: {
+//     openDashboard: function(id) {
+//       console.log(id)
+//     },
+//     addDashboard: function() {
+//       this.$set(this.dashboards, this.newId, { id: this.newId, icon: 'view_module', text: 'Refactuiton2' })
+//       this.newId = (parseInt(this.newId)+1).toString()
+//     },
+//     editDashboard: function() {
+//       this.drawerRight = !this.drawerRight
+//     },
+//     removeDashboard: function(id) {
+//       this.$delete(this.dashboards, id)
+//     }
+//   }
+// }
+
+
 export default {
-  // fromMobx: {
-  //   testModel
-  // },
-  data: () => ({
-    dialog: false,
-    drawer: null,
-    drawerRight: null,
-    newId: '3',
-    dashboards: {
-      '1': { id: '1', icon: 'view_quilt', text: 'Intuition' },
-      '2': { id: '2', icon: 'view_list', text: 'Refactuiton' }
-    }
-  }),
-  props: {
-    source: String
+  data() {
+    return {
+      isCollapse: true
+    };
   },
   methods: {
-    openDashboard: function(id) {
-      console.log(id)
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
     },
-    addDashboard: function() {
-      this.$set(this.dashboards, this.newId, { id: this.newId, icon: 'view_module', text: 'Refactuiton2' })
-      this.newId = (parseInt(this.newId)+1).toString()
-    },
-    editDashboard: function() {
-      this.drawerRight = !this.drawerRight
-    },
-    removeDashboard: function(id) {
-      this.$delete(this.dashboards, id)
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
 }
