966fc26879cdeb3afa4f35bf383115729139f477
Eugen Soloviov <suenot@gmail.com>
2019-01-14 20:52:56 +0300

    react-client: start demo for my trades

---
+++ react-client/src/core_components/MyTrades/Demo.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    uuid: '1',
+    order: 1,
+    datetime: new Date,
+    symbol: 'BTC',
+    type: 'sell',
+    side: 'sell',
+    price: 8888.8888,
+    amount: 77777,
+    cost: 999.99,
+    fee: {
+      cost: 111999.888888,
+      currency: '222'
+    }
+  },
+  {
+    uuid: '2',
+    order: 2,
+    datetime: new Date,
+    symbol: 'BTC',
+    type: 'sell',
+    side: 'sell',
+    price: 8888.8888,
+    amount: 77777,
+    cost: 999.99,
+    fee: {
+      cost: 111999.888888,
+      currency: '222'
+    }
+  },
+  {
+    uuid: '3',
+    order: 3,
+    datetime: new Date,
+    symbol: 'BTC',
+    type: 'sell',
+    side: 'sell',
+    price: 8888.8888,
+    amount: 77777,
+    cost: 999.99,
+    fee: {
+      cost: 111999.888888,
+      currency: '222'
+    }
+  },
+  {
+    uuid: '4',
+    order: 4,
+    datetime: new Date,
+    symbol: 'BTC',
+    type: 'sell',
+    side: 'sell',
+    price: 8888.8888,
+    amount: 77777,
+    cost: 999.99,
+    fee: {
+      cost: 111999.888888,
+      currency: '222'
+    }
+  }
+]
--- react-client/src/core_components/MyTrades/MyTrades.js
+++ react-client/src/core_components/MyTrades/MyTrades.js
@@ -2,57 +2,60 @@ import React from 'react'
 import _ from 'lodash'
 import { observer } from 'mobx-react'
 import moment from 'moment'
-import Preloader from '../Preloader'
+// import Preloader from '../Preloader'
+import Demo from './Demo'
 
-import MyTradesStore from '../../stores/MyTradesStore'
+import MyTradesStore from 'stores/MyTradesStore'
 
 @observer
 class MyTrades extends React.Component {
   render() {
     const {stock, pair} = this.props.data
     var key = `${stock}--${pair}`
-    if (MyTradesStore.myTrades[key] === undefined) {
-      return <Preloader />
-    } else {
-      return (
-        <div>
-          <table className="simpleTable">
-            <thead>
-              <tr>
-                <th>id</th>
-                <th>date</th>
-                <th>symbol</th>
-                <th>type</th>
-                <th>side</th>
-                <th>price</th>
-                <th>amount</th>
-                <th>cost</th>
-                <th>fee</th>
-              </tr>
-            </thead>
-            <tbody>
-            {
-              _.map(MyTradesStore.myTrades[key], (item) => {
-                return (
-                  <tr key={item.uuid}>
-                    <td>{item['order']}</td>
-                    <td>{moment(item['datetime']).format('DD.MM.YY HH:mm:ss')}</td>
-                    <td>{item['symbol']}</td>
-                    <td>{item['type']}</td>
-                    <td>{item['side']}</td>
-                    <td>{item['price'].toFixed(8)}</td>
-                    <td>{item['amount']}</td>
-                    <td>{item['cost']}</td>
-                    <td>{item['fee']['cost'].toFixed(8)} {item['fee']['currency']}</td>
-                  </tr>
-                )
-              })
-            }
-            </tbody>
-          </table>
-        </div>
-      )
+    var data = JSON.parse(JSON.stringify(MyTradesStore.myTrades[key]))
+    var demoMode = false
+    if (data === undefined || _.isEmpty(data) || data.length === 0 ) {
+      data = Demo
+      demoMode = true
     }
+    return (
+      <div>
+        <table className="simpleTable">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>date</th>
+              <th>symbol</th>
+              <th>type</th>
+              <th>side</th>
+              <th>price</th>
+              <th>amount</th>
+              <th>cost</th>
+              <th>fee</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            _.map(data, (item) => {
+              return (
+                <tr key={item.uuid}>
+                  <td>{item['order']}</td>
+                  <td>{moment(item['datetime']).format('DD.MM.YY HH:mm:ss')}</td>
+                  <td>{item['symbol']}</td>
+                  <td>{item['type']}</td>
+                  <td>{item['side']}</td>
+                  <td>{item['price'].toFixed(8)}</td>
+                  <td>{item['amount']}</td>
+                  <td>{item['cost']}</td>
+                  <td>{item['fee']['cost'].toFixed(8)} {item['fee']['currency']}</td>
+                </tr>
+              )
+            })
+          }
+          </tbody>
+        </table>
+      </div>
+    )
   }
   componentWillMount() {
     MyTradesStore.count(1, this.props.data)
