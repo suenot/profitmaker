a3ccc6c151efe871aa3b9545a68fbb8d6f6e06e9
Eugen Soloviov <suenot@gmail.com>
2019-01-15 06:55:35 +0300

    react-client: demo mode for balance pie

--- react-client/src/core_components/BalancePie/BalancePie.js
+++ react-client/src/core_components/BalancePie/BalancePie.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import Preloader from '../Preloader'
 import ReactEcharts from 'echarts-for-react'
+import _ from 'lodash'
+import Demo from './Demo'
 
 import BalanceStore from 'stores/BalanceStore'
 
@@ -10,19 +12,25 @@ class BalancePie extends React.Component {
   render() {
     const {type, stock} = this.props.data
     const key = `${type}--${stock}`
-    if (
-        BalanceStore.balance[key] === undefined
-        || JSON.stringify(BalanceStore.balance[key]) === '{}'
-        || JSON.stringify(BalanceStore.balance[key].data) === '[]'
-      ) {
-      return <Preloader />
+    var data = BalanceStore.balance[key]
+    var demoMode = false
+    if (data === undefined || _.isEmpty(data) || data.length === 0 ) {
+      data = Demo
+      demoMode = true
     }
+    // if (
+    //     BalanceStore.balance[key] === undefined
+    //     || JSON.stringify(BalanceStore.balance[key]) === '{}'
+    //     || JSON.stringify(BalanceStore.balance[key].data) === '[]'
+    //   ) {
+    //   return <Preloader />
+    // }
     var legendData = []
     var seriesData = []
     var selected = {}
-    var totalUSD = BalanceStore.balance[key].totalUSD
+    var totalUSD = data.totalUSD
     var otherUSD = 0
-    BalanceStore.balance[key].data.forEach(function(coin){
+    data.data.forEach(function(coin){
       if (coin.totalUSD !== 0) {
         if ( (coin.totalUSD/totalUSD*100 ) > 5) {
           seriesData.push({
@@ -51,42 +59,45 @@ class BalancePie extends React.Component {
     }
     var option = {
       tooltip : {
-          trigger: 'item',
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
       },
       legend: {
-          type: 'scroll',
-          orient: 'vertical',
-          right: 10,
-          top: 20,
-          bottom: 20,
-          data: optionData.legendData,
-          selected: optionData.selected
+        type: 'scroll',
+        orient: 'vertical',
+        right: 10,
+        top: 20,
+        bottom: 20,
+        data: optionData.legendData,
+        selected: optionData.selected
       },
       series : [
-          {
-              name: 'balance',
-              type: 'pie',
-              radius : '55%',
-              center: ['40%', '50%'],
-              data: optionData.seriesData,
-              itemStyle: {
-                  emphasis: {
-                      shadowBlur: 10,
-                      shadowOffsetX: 0,
-                      shadowColor: 'rgba(0, 0, 0, 0.5)'
-                  }
-              }
+        {
+          name: 'balance',
+          type: 'pie',
+          radius : '55%',
+          center: ['40%', '50%'],
+          data: optionData.seriesData,
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
           }
+        }
       ]
     }
     return (
-      <ReactEcharts
-        option={option}
-        style={{width: '100%', height: '100%', position: 'absolute'}}
-        className='react_for_echarts'
-        theme={'light'}
-      />
+      <div>
+        <ReactEcharts
+          option={option}
+          style={{width: '100%', height: '100%', position: 'absolute'}}
+          className='react_for_echarts'
+          theme={'light'}
+        />
+        { demoMode && <div class="demo">Demo mode: needed API key</div> }
+      </div>
     )
   }
   componentDidMount() {
---
+++ react-client/src/core_components/BalancePie/Demo.js
@@ -0,0 +1,13 @@
+export default {
+  totalUSD: 50000,
+  data: [
+    {
+      shortName: 'BTC',
+      totalUSD: 10000,
+    },
+    {
+      shortName: 'ETH',
+      totalUSD: 40000,
+    }
+  ],
+}
