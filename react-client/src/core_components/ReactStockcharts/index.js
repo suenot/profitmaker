767c81d4d354165073644b5798b1b975a8843775
Eugen Soloviov <suenot@gmail.com>
2019-02-03 22:48:25 +0300

    react: prevent error in react stock charts if sizes small

--- react-client/src/core_components/ReactStockcharts/index.js
+++ react-client/src/core_components/ReactStockcharts/index.js
@@ -9,31 +9,45 @@ import OhlcvStore from 'stores/OhlcvStore'
 
 @observer
 export default class ChartComponent extends React.Component {
-	render() {
-    var {dashboardId, widgetId} = this.props.data
-
-    var key = this.getKey()
-
-    if (
-      OhlcvStore.ohlcvComputed === undefined ||
-      JSON.stringify(OhlcvStore.ohlcvComputed) === '{}' ||
-      OhlcvStore.ohlcvComputed[key] === undefined ||
-      JSON.stringify(OhlcvStore.ohlcvComputed[key]) === '[]' ||
-      JSON.parse( JSON.stringify(OhlcvStore.ohlcvComputed[key]) ).length < 3 ) {
-			return <Preloader />
-		} else {
-      var ordersJSON = JSON.parse( JSON.stringify(OhlcvStore.ohlcvComputed[key]) )
-
-			ordersJSON = ordersJSON.map(function(order){
-				order.date = new Date(order.date)
-				return order
-      })
-
-			return (
-        <Chart id={`${dashboardId}_${widgetId}_chart`} type="hybrid" data={ordersJSON} _data={this.props.data} />
-			)
-		}
+  constructor () {
+    super()
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null
+    }
   }
+
+  componentDidCatch (error, info) {
+    this.setState({ hasError: true, error, errorInfo: info })
+  }
+
+	render() {
+    if (this.state.hasError) {
+      return <div></div>
+    } else {
+      var {dashboardId, widgetId} = this.props.data
+      var key = this.getKey()
+      if (
+        OhlcvStore.ohlcvComputed === undefined ||
+        JSON.stringify(OhlcvStore.ohlcvComputed) === '{}' ||
+        OhlcvStore.ohlcvComputed[key] === undefined ||
+        JSON.stringify(OhlcvStore.ohlcvComputed[key]) === '[]' ||
+        JSON.parse( JSON.stringify(OhlcvStore.ohlcvComputed[key]) ).length < 3 ) {
+        return <Preloader />
+      } else {
+        var ordersJSON = JSON.parse( JSON.stringify(OhlcvStore.ohlcvComputed[key]) )
+        ordersJSON = ordersJSON.map(function(order){
+          order.date = new Date(order.date)
+          return order
+        })
+        return (
+          <Chart id={`${dashboardId}_${widgetId}_chart`} type="hybrid" data={ordersJSON} _data={this.props.data} />
+        )
+      }
+    }
+  }
+
   getKey() {
     try {
       var {stock, pair, timeframe, url} = this.props.data
