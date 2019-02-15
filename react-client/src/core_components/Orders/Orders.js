6ac638262ceb2d3376f161c73eb78ba53889dfa2
Eugen Soloviov <suenot@gmail.com>
2019-02-15 14:09:19 +0300

    react: orders both

--- react-client/src/core_components/Orders/Orders.js
+++ react-client/src/core_components/Orders/Orders.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import _ from 'lodash'
 import Preloader from '../Preloader'
+import ReactDOM from 'react-dom'
 
 import OrdersStore from 'stores/OrdersStore'
 import CreateOrderStore from 'stores/CreateOrderStore'
@@ -12,25 +13,84 @@ class Orders extends React.Component {
     const {type, stock, pair, visualMode, visualModeMax, visualModeCrocodileMax, visualModeWallsMax} = this.props.data
     var [coinFrom, coinTo] = pair.split('_')
     var key = `${stock}--${pair}`
-    var color = type === 'asks' ? 'rgba(255, 138, 138, 0.42)' : 'rgba(78, 136, 71, 0.42)'
-    if (OrdersStore.orders[key] === undefined || OrdersStore.orders[key][type] === undefined) {
+    // var color = type === 'asks' ? 'rgba(255, 138, 138, 0.42)' : 'rgba(78, 136, 71, 0.42)'
+    if (OrdersStore.orders[key] === undefined || OrdersStore.orders[key]['asks'] === undefined || OrdersStore.orders[key]['bids'] === undefined) {
 			return <Preloader />
-		}
+    }
+    var asks = OrdersStore.orders[key]['asks'].slice(0, 30)
+    if (type === 'both') {
+      asks = _.reverse(_.clone(asks))
+    }
     return (
-      <div>
+      <div id={type === 'both' ? 'orders-both' : ''}>
         <table className="simpleTable">
-          <thead>
-            <tr>
-              <th className="simpleTable-header">price <span className="muted">{coinTo}</span></th>
-              <th className="simpleTable-header">amount <span className="muted">{coinFrom}</span></th>
-              <th className="simpleTable-header">total <span className="muted">{coinTo}</span></th>
-              <th className="simpleTable-header">sum <span className="muted">{coinTo}</span></th>
-            </tr>
-          </thead>
+
+          { (type !== 'both') &&
+            <thead>
+                <tr>
+                  <th className="simpleTable-header">price <span className="muted">{coinTo}</span></th>
+                  <th className="simpleTable-header">amount <span className="muted">{coinFrom}</span></th>
+                  <th className="simpleTable-header">total <span className="muted">{coinTo}</span></th>
+                  <th className="simpleTable-header">sum <span className="muted">{coinTo}</span></th>
+                </tr>
+            </thead>
+          }
+
           <tbody>
             {
-              _.map(OrdersStore.orders[key][type].slice(0, 30), (order) => {
+              (type === 'both' || type === 'asks') &&
+              _.map(asks, (order) => {
+                // var _type = 'asks'
                 var percent = 0
+                var color = 'rgba(255, 138, 138, 0.42)'
+                if (visualMode !== 'none') {
+                  if (visualModeMax === 'total sum') {
+                    percent = visualMode === 'crocodile' ? order.sumPercent : order.totalPercent
+                  } else { // fixed
+                    if (visualMode === 'crocodile') {
+                      if (visualModeCrocodileMax >= order.total) percent = 100
+                      percent = order.sum / visualModeCrocodileMax * 100
+                    } else { // wall
+                      if (visualModeWallsMax >= order.total) percent = 100
+                      percent = order.total / visualModeWallsMax * 100
+                    }
+                  }
+                }
+
+                var percentInverse = 100 - percent
+                var percentInverseToFixed = percentInverse.toFixed(2)
+                return <tr
+                  key={order.id}
+                  onClick={this.setAll.bind(this, order.price, order.amount, order.total)}
+                  style={{background: `linear-gradient(to right, #ffffff 0%, #ffffff ${percentInverseToFixed}%, ${color} ${percentInverseToFixed}%, ${color} 100%)`}}
+                >
+                  <td>{order.price.toFixed(8)}</td>
+                  <td>{order.amount.toFixed(8)}</td>
+                  <td>{order.total.toFixed(8)}</td>
+                  <td>{order.sum.toFixed(8)}</td>
+                </tr>
+              })
+            }
+
+            { (type === 'both') &&
+              <tr
+                className="orders-center"
+                ref={input => {
+                  this.ordersCenter = input;
+                }}
+                >
+                <th className="simpleTable-header">price <span className="muted">{coinTo}</span></th>
+                <th className="simpleTable-header">amount <span className="muted">{coinFrom}</span></th>
+                <th className="simpleTable-header">total <span className="muted">{coinTo}</span></th>
+                <th className="simpleTable-header">sum <span className="muted">{coinTo}</span></th>
+              </tr>
+            }
+
+            {
+              (type === 'both' || type === 'bids') &&
+              _.map(OrdersStore.orders[key]['bids'].slice(0, 30), (order) => {
+                var percent = 0
+                var color = 'rgba(78, 136, 71, 0.42)'
 
                 if (visualMode !== 'none') {
                   if (visualModeMax === 'total sum') {
@@ -67,15 +127,27 @@ class Orders extends React.Component {
   }
   setAll(price, amount, total) {
     const {stock, pair} = this.props.data
+
     var key = `${stock}--${pair}--buy`
     CreateOrderStore.setPrice(price, key)
     CreateOrderStore.setAmount(amount, key)
     CreateOrderStore.setTotal(total, key)
-    key = `${stock}--${pair}--sell`
+
+    var key = `${stock}--${pair}--sell`
     CreateOrderStore.setPrice(price, key)
     CreateOrderStore.setAmount(amount, key)
     CreateOrderStore.setTotal(total, key)
   }
+  componentDidMount() {
+    if (this.props.data.type === 'both') {
+      setTimeout(()=> {
+        var widgetHeight = ReactDOM.findDOMNode(this).parentNode.parentNode.parentNode.offsetHeight
+        var top = ReactDOM.findDOMNode(this).querySelector('.orders-center').offsetTop
+        ReactDOM.findDOMNode(this).parentNode.scrollTop = top - widgetHeight / 2 + 24
+      }, 700)
+    }
+  }
+
   componentWillMount() {
     OrdersStore.count(1, this.props.data)
   }
