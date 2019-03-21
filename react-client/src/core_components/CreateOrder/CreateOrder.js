7bea893f12e0aa5d1403dab0b98c4e6997f91779
Eugen Soloviov <suenot@gmail.com>
2019-03-22 00:13:30 +0300

    react: createOrder: refactoring mobx

--- react-client/src/core_components/CreateOrder/CreateOrder.js
+++ react-client/src/core_components/CreateOrder/CreateOrder.js
@@ -3,26 +3,40 @@ import { observer } from 'mobx-react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import InputAdornment from '@material-ui/core/InputAdornment'
+import axios from 'axios'
+import Alert from 'react-s-alert'
 
-import CreateOrderStore from 'stores/CreateOrderStore'
+// import CreateOrderStore from 'stores/CreateOrderStore'
 import BalanceStore from 'stores/BalanceStore'
 
 @observer
 class CreateOrder extends React.Component {
+  state = {
+    interval: '',
+    data: [],
+    form: { price: "", amount: "", total: ""},
+    available: {
+      buy: 0,
+      sell: 0
+    }
+  }
   render() {
-    const {stock, pair, type, accountId} = this.props.data
-    const key = `${stock}--${pair}--${type}--${accountId}`
-    if (CreateOrderStore.form[key] === undefined) this.initForm(key)
+    const {pair, type, stock, accountId} = this.props.data
+    var form = this.state.form
+    // var available = this.state.available
+    // var available = this.state.available
+    var key = `${type}--${stock}--${accountId}`
+    var available = BalanceStore.available(stock, pair, accountId)
     return (
       <div className="simpleForm">
         <div className="createOrder-header">
-          Available: {(type === 'buy' ? BalanceStore.available(stock, pair).buy : BalanceStore.available(stock, pair).sell).toFixed(8)} {type === 'buy' ? pair.split('_')[1] : pair.split('_')[0]}
+          Available: {(type === 'buy' ? available.buy : available.sell).toFixed(8)} {type === 'buy' ? pair.split('_')[1] : pair.split('_')[0]}
         </div>
         <div className="simpleForm-formGroup">
           <TextField
               id="outlined-name"
               label="Price"
-              value={CreateOrderStore.form[key].price}
+              value={form.price}
               onChange={this.changeValue.bind(this, 'price')}
               variant="outlined"
               fullWidth
@@ -35,7 +49,7 @@ class CreateOrder extends React.Component {
           <TextField
               id="outlined-name"
               label="Amount"
-              value={CreateOrderStore.form[key].amount}
+              value={form.amount}
               onChange={this.changeValue.bind(this, 'amount')}
               variant="outlined"
               fullWidth
@@ -48,7 +62,7 @@ class CreateOrder extends React.Component {
           <TextField
               id="outlined-name"
               label="Total"
-              value={CreateOrderStore.form[key].total}
+              value={form.total}
               onChange={this.changeValue.bind(this, 'total')}
               variant="outlined"
               fullWidth
@@ -64,36 +78,100 @@ class CreateOrder extends React.Component {
     )
   }
   changeValue(field, value) {
-    const {stock, pair, type, accountId} = this.props.data
-    // console.log(value)
-    CreateOrderStore.createChange(stock, pair, type, accountId, field, value.target.value)
+    value = value.target.value
+    var amount
+    var total
+    var form = this.state.form
+    if (field === 'price') {
+      form.price = value
+      total = (parseFloat(form.price) * parseFloat(form.amount)).toFixed(8)
+      form.total = total !== 'NaN' ? total : ''
+    } else if (field === 'amount') {
+      form.amount = value
+      total = (parseFloat(form.price) * parseFloat(form.amount)).toFixed(8)
+      form.total = (total !== 'NaN') ? total : ''
+    } else if (field === 'total') {
+      form.total = value
+      var amount = (parseFloat(form.total) / parseFloat(form.price)).toFixed(8)
+      form.amount = amount !== 'NaN' ? amount : ''
+    }
+    this.setState({
+      form: form
+    })
   }
+
   createOrder() {
     const {stock, pair, type, accountId} = this.props.data
-    CreateOrderStore.createOrder(stock, pair, type, accountId)
+    var createMsg = 'creating ' + type + ' order on ' + stock + ': '+ pair + ' price: '+this.state.form.price + ' amount: ' + this.state.form.amount
+    Alert.warning(createMsg)
+    axios.post(`/user-api/createOrder`, {
+      'stock': stock,
+      'accountId': accountId,
+      'pair': pair,
+      'type': type,
+      'price': this.state.form.price,
+      'amount': this.state.form.amount
+    })
+    .then((response) => {
+      Alert.success('orderCreated')
+    })
+    .catch((error) => {
+      var errorMessage = 'Server is not available'
+      try {
+        errorMessage = error.response.data.error
+      } catch(err) {}
+      Alert.error(errorMessage)
+    })
   }
-  componentDidMount() {
-    // BalanceStore.count(1, this.props.data)
-    // TODO: fix thix hack
-    setTimeout(()=>{
-      this.forceUpdate()
-    }, 2000)
-  }
-  // componentWillMount() {
-  //   BalanceStore.count(1, this.props.data)
+
+  // setPrice(price) {
+  //   var form = this.state.form
+  //   if (form === undefined) form = { price: "", amount: "", total: ""}
+  //   form.price = price
+  //   form.price = price
   // }
+
+  // setAmount(amount) {
+  //   var form = this.state.form
+  //   if (form === undefined) form = { price: "", amount: "", total: ""}
+  //   form.amount = amount
+  //   form.amount = amount
+  // }
+
+  // setTotal(total) {
+  //   var form = this.state.form
+  //   if (form === undefined) form = { price: "", amount: "", total: ""}
+  //   form.total = total.toFixed(8)
+  //   form.total = total.toFixed(8)
+  // }
+
+  // componentDidMount() {
+  //   const {stock, type, pair, accountId} = this.props.data
+  //   var key = `${type}--${stock}--${accountId}`
+  //   BalanceStore.fetchBalance(stock, key, type, accountId)
+  //   setTimeout(()=>{
+  //     BalanceStore.fetchBalance(stock, key, type, accountId)
+  //   }, 3000)
+
+  //   this.setState({
+  //     interval: setInterval(()=>{
+  //       var available = BalanceStore.available(stock, pair, accountId)
+  //       console.log(available)
+  //       if (JSON.stringify(available) !== JSON.stringify(this.state.available)) {
+  //         this.setState({
+  //           available: available
+  //         })
+  //       }
+  //     }, 1000)
+  //   })
+  // }
+
   // componentWillUnmount() {
-  //   BalanceStore.count(-1, this.props.data)
+  //   if (this.state.interval) {
+  //     clearInterval(this.state.interval)
+  //     this.setState({ timer: null })
+  //   }
   // }
-  // componentWillUpdate() {
-  //   BalanceStore.count(-1, this.props.data)
-  // }
-  // componentDidUpdate() {
-  //   BalanceStore.count(1, this.props.data)
-  // }
-  initForm(key) {
-    CreateOrderStore.initForm(key)
-  }
 }
 
 export default CreateOrder
