7e3e1371d38dd288999f97f64f6416bc838da0aa
Eugen Soloviov <suenot@gmail.com>
2019-03-22 00:25:17 +0300

    react: Pairs: refactoring mobx

--- react-client/src/core_components/Pairs/Pairs.js
+++ react-client/src/core_components/Pairs/Pairs.js
@@ -3,16 +3,26 @@ import React from 'react'
 import _ from 'lodash'
 import { observer } from 'mobx-react'
 import CloseIcon from '@material-ui/icons/Close'
-import Divider from '@material-ui/core/Divider'
 import PerfectScrollbar from 'react-perfect-scrollbar'
+import axios from 'axios'
 
-import PairsStore from 'stores/PairsStore'
 import DrawersStore from 'stores/DrawersStore'
 
 @observer
 class Pairs extends React.Component {
+  state = {
+    interval: '',
+    tube: '',
+    hash: '',
+    data: [],
+    timer: 1000,
+    serverBackend: 'https://kupi.network',
+    filter: '',
+  }
+
   render() {
     var {stock, drawer} = this.props.data
+    var data = this.state.data
     return (
       <div className="drawer">
         <div className="drawer-title">
@@ -24,7 +34,7 @@ class Pairs extends React.Component {
           <table className="simpleTable">
             <tbody>
               {
-                _.map(PairsStore.pairsComputed[stock], (pair) => {
+                _.map(data, (pair) => {
                   return <tr key={pair}>
                     <td>
                       <div className="cell" onClick={this.setPair.bind(this, pair)}>
@@ -41,28 +51,93 @@ class Pairs extends React.Component {
     )
   }
   toggleFilter(e) {
-    PairsStore.setPairsFilter(e.target.value)
+    this.setState({
+      filter: e.target.value
+    })
+    this.fetchPairs()
   }
   setPair(pair) {
     var group = this.props.data.group
-    // PairsStore.setPair(pair)
     DashboardsStore.setWidgetsData('pair', pair, group)
   }
-  componentWillMount() {
-    PairsStore.count(1, this.props.data)
-  }
-  componentWillUnmount() {
-    PairsStore.count(-1, this.props.data)
-  }
-  componentWillUpdate() {
-    PairsStore.count(-1, this.props.data)
-  }
-  componentDidUpdate() {
-    PairsStore.count(1, this.props.data)
-  }
+
   drawerClose(drawer) {
     DrawersStore.drawerClose(drawer)
   }
+
+  async fetchPairs_kupi(stockLowerCase) {
+    return axios.get(`${this.state.serverBackend}/api/${stockLowerCase}/pairs/`)
+    .then((response) => {
+      return response.data
+    })
+    .catch(() => {
+      this.state.tube = 'ccxt'
+      return []
+    })
+  }
+
+  async fetchPairs_ccxt(stockLowerCase) {
+    return axios.get(`/user-api/ccxt/${stockLowerCase}/pairs/`)
+    .then((response) => {
+      return response.data
+    })
+    .catch(() => {
+      return []
+    })
+  }
+
+  async fetchPairs() {
+    const {stock} = this.props.data
+    var stockLowerCase = stock.toLowerCase()
+    var data
+    if (this.state.tube === 'ccxt') {
+      data = await this.fetchPairs_ccxt(stockLowerCase)
+    } else {
+      data = await this.fetchPairs_kupi(stockLowerCase)
+    }
+
+    data = data.map((pair) => {
+      return pair.split('/').join('_')
+    })
+
+    data = data.filter((pair) => {
+      return pair.toLowerCase().indexOf( this.state.filter.toLowerCase() ) !== -1
+    })
+
+
+    if (this.hash === JSON.stringify(data)) return true
+    this.hash = JSON.stringify(data)
+
+    this.setState({
+      data: data
+    })
+  }
+
+  start() {
+    this.setState({
+      interval: setInterval(()=>{
+        this.fetchPairs()
+      }, this.state.timer)
+    })
+  }
+  finish() {
+    if (this.state.interval) {
+      clearInterval(this.state.interval)
+      this.setState({ interval: null })
+    }
+  }
+  componentDidMount() {
+    this.start()
+  }
+  componentWillUnmount() {
+    this.finish()
+  }
+  // componentWillUpdate() {
+  //   this.finish()
+  // }
+  // componentDidUpdate() {
+  //   this.start()
+  // }
 }
 
 export default Pairs
