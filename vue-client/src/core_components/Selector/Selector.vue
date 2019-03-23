3aa30e1a4fea2929b90f07e6e37463ead2ea838f
Eugen Soloviov <suenot@gmail.com>
2019-03-23 21:36:17 +0300

    vue: selector

---
+++ vue-client/src/core_components/Selector/Selector.vue
@@ -0,0 +1,63 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <td>
+          {{stock}}
+          <span v-if="accountId" class="muted">
+            <br/>{{accountId}}
+          </span>
+        </td>
+        <td>{{pair}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import Store from '../../stores/Store'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  fromMobx: {
+    stock: {
+      get() {
+        return Store.stock
+      }
+    },
+    pair: {
+      get() {
+        return Store.pair
+      }
+    },
+    accountId: {
+      get() {
+        return Store.accountId
+      }
+    }
+  },
+  created() {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+table
+  width: 100%
+  border-collapse: collapse
+  td
+    padding: 30px
+    border: 1px solid rgba(0,0,0,0.12)
+    text-align: center
+    &:hover
+      cursor: pointer
+      background: rgba(0,0,0,0.08)
+
+</style>
