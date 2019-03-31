e58ebd3331d88ca0795bc26de1ec880b39bdc4f3
Eugen Soloviov <suenot@gmail.com>
2019-03-31 23:14:52 +0300

    vue: accounts list

--- vue-client/src/utils_components/Profile/Accounts.vue
+++ vue-client/src/utils_components/Profile/Accounts.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="accounts">
-    {{accounts}}
+    <el-table
+      :data="accountsComputed"
+      style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <p>Id: {{ props.row.id }}</p>
+          <p>Parser: {{ props.row.parser }}</p>
+          <p>Withdraw: {{ props.row.withdrawLimit }} {{ props.row.withdrawLimitIn }}</p>
+          <p>
+            Keys:
+            <ul>
+              <li v-for="(value, index) in props.row.keys" :key="index">{{value}}</li>
+            </ul>
+          </p>
+          <p>Note: {{ props.row.note }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="Name"
+        prop="name">
+      </el-table-column>
+      <el-table-column
+        label="Stock"
+        prop="stock">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -13,13 +38,20 @@ export default {
       accounts: require('./data.js').default
     }
   },
-  // fromMobx: {
-  //   accounts: {
-  //     get() {
-  //       return AccountsStore.accounts
-  //     }
-  //   },
-  // },
+  computed: {
+    accountsComputed() {
+      var data = _.cloneDeep(this.accounts)
+      data =  Object.values(data)
+      data = data.map((account)=>{
+        account.keys = []
+        if (account.safe) account.keys.push('safe')
+        if (account.notSafe) account.keys.push('not safe')
+        if (account.danger) account.keys.push('danger')
+        return account
+      })
+      return data
+    }
+  }
 }
 </script>
 
