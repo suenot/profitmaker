5f47e13c3c2b69fd2a7f866690dc0f3d88727ff6
Eugen Soloviov <suenot@gmail.com>
2019-04-12 16:33:24 +0300

    vue: start signal history

---
+++ vue-client/src/core_components/Signals/History/History.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ve-line :data="chartData" :settings="chartSettings"></ve-line>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import VeLine from 'v-charts/lib/line.common'
+import moment from 'moment-timezone'
+
+
+export default {
+  components: { VeLine },
+  data () {
+    return {
+      server: 'https://kupi.network/api/',
+      chartSettings: {
+        axisSite: { right: ['profitUSD'] },
+        yAxisType: ['normal', 'KMB' ],
+        yAxisName: ['percent', 'profitUSD']
+      },
+      chartData: {
+        columns: ['updated', 'percent', 'profitUSD'],
+        rows: [
+
+        ]
+      }
+    }
+  },
+  mounted() {
+    axios.get(`${this.server}/signal-history/${this.$route.params.id}`)
+    .then((response) => {
+      var data = response.data
+      data.forEach(function(item){
+        item.timestamp = moment(item.timestamp).format()
+      })
+      this.chartData.rows = data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
