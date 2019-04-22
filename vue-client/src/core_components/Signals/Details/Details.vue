870dd83011460fc9f1efa8e9560747611a030115
Eugen Soloviov <suenot@gmail.com>
2019-04-22 21:50:27 +0300

    vue: editing signal details

--- vue-client/src/core_components/Signals/Details/Details.vue
+++ vue-client/src/core_components/Signals/Details/Details.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="detailsWrap">
-    <div>
-      <div>full_name_from</div>
-      <el-input placeholder="coiFrom" v-model="from.full_name" class="m-16"></el-input>
-      <el-checkbox v-model="from.white">White</el-checkbox>
-      <el-checkbox v-model="from.black">Black</el-checkbox>
-      <el-checkbox v-model="from.favorite">Favorite</el-checkbox>
-      <div>Note</div>
-      <el-input type="textarea" v-model="from.note"></el-input>
+  <div>
+    {{data}}
+    <div class="detailsWrap">
+      <div>
+        <el-checkbox v-model="from.white">White</el-checkbox>
+        <el-checkbox v-model="from.black">Black</el-checkbox>
+        <el-checkbox v-model="from.favorite">Favorite</el-checkbox>
+        <div>Full name</div>
+        <el-input placeholder="Full name" v-model="from.full_name" class="m-16" @change="changeData($event)"></el-input>
+        <div>Note</div>
+        <el-input type="textarea" v-model="from.note" @change="changeData($event)"></el-input>
+      </div>
+      <div>
+        <el-checkbox v-model="to.white">White</el-checkbox>
+        <el-checkbox v-model="to.black">Black</el-checkbox>
+        <el-checkbox v-model="to.favorite">Favorite</el-checkbox>
+        <div>Full name</div>
+        <el-input placeholder="Full name" v-model="to.full_name" class="m-16" @change="changeData($event)"></el-input>
+        <div>Note</div>
+        <el-input type="textarea" v-model="to.note" @change="changeData($event)"></el-input>
+      </div>
     </div>
-    <div>
-      <div>full_name_to</div>
-      <el-input placeholder="coiTo" v-model="to.full_name" class="m-16"></el-input>
-      <el-checkbox v-model="to.white">White</el-checkbox>
-      <el-checkbox v-model="to.black">Black</el-checkbox>
-      <el-checkbox v-model="to.favorite">Favorite</el-checkbox>
-      <div>Note</div>
-      <el-input type="textarea" v-model="to.note"></el-input>
-
-
-    </div>
-
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
+      server: 'https://kupi.network/api/',
+      data: {},
       from: {
         full_name: '',
         white: true,
@@ -44,8 +47,40 @@ export default {
       }
     }
   },
-  mounted() {},
-
+  mounted() {
+    this.getData()
+    this.getDetails()
+  },
+  methods: {
+    getDetails() {
+      axios.get(`${this.server}/intuition-details/${this.$route.params.id}`)
+      .then((response) => {
+        this.data = response.data
+      })
+      .catch((error) => {
+      })
+    },
+    getData() {
+      axios.get(`${this.server}/pairs/BINANCE--ETH_BTC`)
+      .then((response) => {
+        this.from = response
+      })
+      .catch((error) => {
+      })
+    },
+    changeData(e) {
+      axios.post(`${this.server}/pairs/core`, {
+        id: 'BINANCE--ETH_BTC',
+        key: 'note',
+        value: e
+      })
+      .then((response) => {
+        this.getData()
+      })
+      .catch((error) => {
+      })
+    }
+  }
 }
 
 </script>
