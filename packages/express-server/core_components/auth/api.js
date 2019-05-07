0c47b7c9c9a058b0c1c808867885f2f64122243c
Eugen Soloviov <suenot@gmail.com>
2019-05-07 20:26:52 +0300

    fix

--- packages/express-server/core_components/auth/api.js
+++ packages/express-server/core_components/auth/api.js
@@ -20,7 +20,6 @@ router.post("/login", (req, res, next) => {
     }
     if (!user) {
       console.log("Cannot log in")
-      console.log(global.USERS)
       return res.status(400).send([user, "Cannot log in", info])
     }
     req.login(user, (err) => {
