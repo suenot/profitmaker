d2ae5ff19b759cc091a0eb54eab92d1879682821
Eugen Soloviov <suenot@gmail.com>
2019-05-29 16:29:35 +0300

    vue: accounting: move my_trades for accounting

---
+++ packages/vue-client/src/core_components/Accounting/MyTrades/MyTrades.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title">
+      <span class="title-actions">
+        <el-button type="default" icon="el-icon-back" circle @click="$router.go(-1)"></el-button>
+      </span>
+      <h3 class="title-header">My trades</h3>
+    </div>
+    <Widget :widget="blocks.MyTradesForDeal"  />
+  </div>
+</template>
+
+<script>
+import Store from '@/stores/Store'
+import { toJS } from 'mobx'
+export default {
+  fromMobx: {
+    blocks: {
+      get() {
+        return toJS(Store.blocks)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.widget
+  min-height: 100vh
+</style>
+
