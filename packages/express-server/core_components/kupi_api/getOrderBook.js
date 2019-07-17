19adae6aa3df73593144b8afff43eb13c7318a3a
Serghey Soloviov <thatmean@gmail.com>
2019-07-17 20:53:31 +0300

    user_components fix

--- packages/express-server/core_components/kupi_api/getOrderBook.js
+++ packages/express-server/core_components/kupi_api/getOrderBook.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const getOrderBook = async function(stock, pair) {
   try {
     // binance, ETH_BTC
-    var response = await axios.get(`http://api.kupi.network/${stock}/orders/${pair}`)
+    var response = await axios.get(`https://api.kupi.network/${stock}/orders/${pair}`)
     return response.data
   } catch (err) { console.log(err) }
 }
--- packages/express-server/user_components/index.js
+++
@@ -1,5 +0,0 @@
-const userComponents = async () => {
-  // console.log('user_components')
-}
-
-module.exports = userComponents
