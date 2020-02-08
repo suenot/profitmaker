3b0d2f299662cfc1cb4d8c43f463b592dcbbbe9f
kht2199 <kht2199@gmail.com>
2020-02-08 17:50:37 +0900

    added packages echarts, v-chart

--- packages/vue-client-ts/.eslintrc.js
+++ packages/vue-client-ts/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': 0,
     'object-curly-spacing': 0,
+    'object-property-newline': 0,
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
